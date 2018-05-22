7b7be13143edd9eaee2b4f960cb050b2a50b894a
leslie <hushuangsoutheastasia@gmail.com>
2018-05-22 23:51:30 +0800

    新增百兆文件下载, 优化移动端显示

--- speedtest/garbage.php
+++ speedtest/garbage.php
@@ -8,7 +8,7 @@ header('HTTP/1.1 200 OK');
 // Download follows...
 header('Content-Description: File Transfer');
 header('Content-Type: application/octet-stream');
-header('Content-Disposition: attachment; filename=random.dat');
+header('Content-Disposition: attachment; filename=100mb.dat');
 header('Content-Transfer-Encoding: binary');
 // Never cache me
 header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
