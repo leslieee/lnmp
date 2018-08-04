cfa63293e02ca8782df048d3c92bc1e4d8a2e369
leslie <hushuangsoutheastasia@gmail.com>
2018-08-04 13:37:48 +0000

    上传禁用文件创建权限

--- upload/index.php
+++ upload/index.php
@@ -11,7 +11,7 @@ error_reporting( error_reporting() & ~E_NOTICE );
 
 //Security options
 $allow_delete = false; // Set to false to disable delete button and delete POST request.
-$allow_create_folder = true; // Set to false to disable folder creation
+$allow_create_folder = false; // Set to false to disable folder creation
 $allow_upload = true; // Set to true to allow upload files
 $allow_direct_link = true; // Set to false to only allow downloads and not direct link
 
